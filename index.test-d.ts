400a2a36627ed2983d223a9d4822d4c76c19686f
Louis Orleans <louis@orleans.io>
2018-10-19 14:11:42 -0700

    👌 change `cwd` in test

--- index.test-d.ts
+++ index.test-d.ts
@@ -4,8 +4,8 @@ import { IPackage } from '.';
 
 (async () => {
 	expectType<IPackage>(await readPkg());
-	expectType<IPackage>(await readPkg({ cwd: './', normalize: false }));
+	expectType<IPackage>(await readPkg({ cwd: '.', normalize: false }));
 
 	expectType<IPackage>(readPkg.sync());
-	expectType<IPackage>(readPkg.sync({ cwd: './', normalize: false }));
+	expectType<IPackage>(readPkg.sync({ cwd: '.', normalize: false }));
 })();
