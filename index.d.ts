91c10d8efc8c8a1946f63e2d33bcd2870b7f9b6f
Louis Orleans <louis@orleans.io>
2018-10-18 18:44:50 -0700

    ✨ add TypeScript typings

---
+++ index.d.ts
@@ -0,0 +1,31 @@
+export interface IOptions {
+	/**
+	 * Directory to start from.
+	 * @default process.cwd()
+	 */
+	cwd: string;
+	/**
+	 * [Normalize](https://github.com/npm/normalize-package-data#what-normalization-currently-entails) the package data.
+	 *
+	 * @default true
+	 */
+	normalize?: boolean;
+}
+
+export interface IPackage {
+	[key: string]: any;
+}
+
+/**
+ * Returns a `Promise` for the parsed JSON.
+ */
+declare function readPkg(options?: IOptions): Promise<IPackage>;
+
+declare namespace readPkg {
+	/**
+	 * Returns the parsed JSON.
+	 */
+	function sync(options?: IOptions): IPackage;
+}
+
+export default readPkg;
