c09b1093845c2d33993d656fe9e3d9374056d9f7
Sindre Sorhus <sindresorhus@gmail.com>
2018-10-22 00:58:50 +0700

    Update index.test-d.ts

--- index.test-d.ts
+++ index.test-d.ts
@@ -4,8 +4,8 @@ import {PackageMetadata} from '.';
 
 (async () => {
 	expectType<PackageMetadata>(await readPkg());
-	expectType<PackageMetadata>(await readPkg({ cwd: '.', normalize: false }));
+	expectType<PackageMetadata>(await readPkg({cwd: '.', normalize: false}));
 
 	expectType<PackageMetadata>(readPkg.sync());
-	expectType<PackageMetadata>(readPkg.sync({ cwd: '.', normalize: false }));
+	expectType<PackageMetadata>(readPkg.sync({cwd: '.', normalize: false}));
 })();
