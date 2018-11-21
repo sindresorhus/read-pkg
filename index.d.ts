0ea93810e96bf31d3fdd394de4aea67ff299c0e9
Sindre Sorhus <sindresorhus@gmail.com>
2018-11-21 14:11:19 +0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -5,6 +5,7 @@ export interface Options {
 	 * @default process.cwd()
 	 */
 	cwd?: string;
+
 	/**
 	 * [Normalize](https://github.com/npm/normalize-package-data#what-normalization-currently-entails) the package data.
 	 *
@@ -21,7 +22,8 @@ export interface PackageMetadata {
  * Returns a `Promise` for the parsed JSON.
  */
 export default function (options?: Options): Promise<PackageMetadata>;
+
 /**
  * Returns the parsed JSON.
  */
-export function sync (options?: Options): PackageMetadata;
+export function sync(options?: Options): PackageMetadata;
